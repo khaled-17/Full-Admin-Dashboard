e01bcc266999dbbcc1ffa65eaf59654a419a366e
ram <khaled_17@agr.sohag.edu.eg>
2023-12-10 01:11:00 +0200

    fix proplem

--- src/page/Updates/Updates.jsx
+++ src/page/Updates/Updates.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
  import { Stack } from '@mui/material';
-import OneAccordion from './oneAccordion';
+import OneAccordion from './OneAccordion';
   
 export default function Updates() {
 
